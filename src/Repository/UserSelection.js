871d8523e0f773654e8a543596a29b94d68254c9
mobin <mobintahmasbi13801202@gmail.com>
2023-04-16 22:39:45 +0430

    remove mysql database from project

--- src/Repository/UserSelection.js
+++ src/Repository/UserSelection.js
@@ -1,36 +1,20 @@
+import sq from "../boot/mysqlconnection.js";
 import Contact from "../models/Contact.js";
-
+const sequelize = sq();
 // this function get phone number and password and return an object
 // the object has message property also has all contacts that user
 // saved
 
 async function getuser(phoneNumber, password) {
-  try {
-    const user = await sequelize.query(
-      `SELECT * FROM users WHERE phoneNumber = '${phoneNumber}' AND password = '${password}'`
-    );
-    if (user.length === 0) {
-      throw Error("Invalid phone number or password");
-    }
-    const contacts = await Contact.findAll({
-      where: { userId: user[0].id },
-      attributes: ["path", "userName"],
-    });
-    return {
-      Userinformation: {
-        userId: user[0].userId,
-        userName: user[0].userName,
-      },
-      status: true,
-      message: "Contacts retrieved successfully",
-      contacts: contacts.map((contact) => ({
-        userName: contact.userName,
-        path: contact.path,
-      })),
-    };
-  } catch (error) {
-    console.error(error);
-    return error
-  }
+  return {
+    Userinformation: {
+      userId: 0,
+      userName: "username",
+    },
+    status: true,
+    message: "Contacts retrieved successfully",
+    contacts: []
+  };
 }
+
 export default getuser;
