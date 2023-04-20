6cd75ac146bc4f2eb33ffb025cb25b0443f2629c
Shayan-Eskandari <shayanss8080@gmail.com>
2023-04-20 19:27:42 +0430

    creat user model

--- src/boot/mogon.js
+++ src/boot/mogon.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const connectionToMongo = () => {
   mongoose.connect("mongodb://127.0.0.1:27017/Chatroom").then(() => {
     console.log("connected");
---
+++ src/models/Users.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema= new Schema({
+    name: {
+        type:String,
+        require :true,
+        min: 3,
+        max:10
+    },
+
+    Phone_number:{
+        type:String,
+        require :true,
+        min :11,
+        max :11
+    },
+    path:String,
+    password:{
+        type:String,
+        require :true,
+        min :8,
+        max :46
+    },
+    contact:[{
+        name: {
+            type:String,
+            require :true,
+            min: 3,
+            max:10
+        },
+
+        Phone_number:{
+            type:String,
+            require :true,
+            min :11,
+            max :11
+        },
+        path:String,
+    }]
+})
+export default  mongoose.model("User",userSchema)
