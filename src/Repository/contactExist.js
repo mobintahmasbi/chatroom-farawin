7158ee3a653b12b2db53edd7ff938a9ae6ffc723
Shayan-Eskandari <shayanss8080@gmail.com>
2023-04-26 11:53:43 +0430

    the contact exist is done.

--- src/Repository/contactExist.js
+++ src/Repository/contactExist.js
@@ -1,9 +1,37 @@
 
 import User from "../models/Users.js";
 async function contactExist(userPhoneNumber, ContactPhoneNumber){
-    return{
-        status:true,
-        message:"this contact is exist"
+    try {
+        const user = await User.findOne({Phone_number:userPhoneNumber})
+        const contact=await  user.findOne({'user.contact.Phone_number':{$elemMatch:ContactPhoneNumber}},function (err, contact) {
+
+            if (err){
+                return {
+                status: false,
+                message:err.message
+                }
+            }
+
+            if (contact) {
+               return {
+               status:true,
+               message: "the contact is exist"
+               }
+            }
+
+
+        else {
+              return {
+              status: false,
+              message: "the contact is not exist"
+              }
+            }
+
+        }
+        )}
+    catch (e){
+        console.log(e.message)
     }
 }
+
 export default contactExist;
