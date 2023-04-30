3d633c40e65c746f6c6101d29a19187f8f4210b9
Omid <omidk7914.ok@gmail.com>
2023-04-30 05:18:19 +0430

    edit window adder

--- src/Views/assets/js/handelChatroom.js
+++ src/Views/assets/js/handelChatroom.js
@@ -1,212 +1,232 @@
 //////get of Server Account and Contacts
 let fetchGetContact = async () => {
-
-    let res = await fetch(`http://localhost:3000/api/v1/chatroom/contacts`)
-    let contss = await res.json()
-    return contss;
-
-}
+  let res = await fetch(`http://localhost:3000/api/v1/chatroom/contacts`);
+  let contss = await res.json();
+  return contss;
+};
 
 let getdb = async () => {
+  let info = await fetchGetContact();
+  userAccount = info.User;
+  acconuntNumber = info.User.phone_number;
+  accountContacts = info.contacts;
+  console.log(info);
 
-    let info = await fetchGetContact()
-    userAccount = info.User
-    acconuntNumber = info.User.phone_number;
-    accountContacts = info.contacts;
-    console.log(info);
+  starter.style.display = "flex";
+  chatroom.style.display = "none";
 
-    starter.style.display = "flex";
-    chatroom.style.display = "none";
-
-    pvListGenerator(accountContacts);
+  pvListGenerator(accountContacts);
 };
 
 function butMoreHandeler(event) {
-    body.removeEventListener("click", bodyhandeler);
-    if (menuMore.style.display == "none") {
-        menuMore.style.display = "flex";
-        menuMore.style.top = event.layerY - 8 + "px";
-        menuMore.style.left = event.layerX + 70 + "px";
-    } else {
-        menuMore.style.display = "none";
-    }
-    setTimeout(() => {
-        body.addEventListener("click", bodyhandeler);
-    }, 100);
+  body.removeEventListener("click", bodyhandeler);
+  if (menuMore.style.display == "none") {
+    menuMore.style.display = "flex";
+    menuMore.style.top = event.layerY - 8 + "px";
+    menuMore.style.left = event.layerX + 70 + "px";
+  } else {
+    menuMore.style.display = "none";
+  }
+  setTimeout(() => {
+    body.addEventListener("click", bodyhandeler);
+  }, 100);
 }
 /////add pv in listPv
-
+let fetchSetContact = async () => {
+  let res = await fetch(`http://localhost:3000/api/v1/chatroom/contacts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    redirect: "follow",
+    body: JSON.stringify({
+      userName: userName.value,
+      phoneNumber: phoneNumber.value,
+      password: password.value,
+    }),
+  });
+};
 let adderNewContact = (enteredValue) => {
-    let newContact = contactsGlobal.find((item) => {
-        return item.number == enteredValue;
+  let newContact = contactsGlobal.find((item) => {
+    return item.number == enteredValue;
+  });
+
+  if (newContact) {
+    let bolianRepeat = accountContacts.find((item) => {
+      return item.number == newContact.number;
     });
 
-    if (newContact) {
-        let bolianRepeat = accountContacts.find((item) => {
-            return item.number == newContact.number;
-        });
-
-        if (bolianRepeat) {
-            console.log("repead");
-        } else {
-            accountContacts.push(newContact);
-
-            setDB(accountContacts);
-
-            pvListGenerator(accountContacts);
-        }
-
-        bolianRepeat = "";
+    if (bolianRepeat) {
+      console.log("repead");
     } else {
-        console.log("na Mojod");
+      accountContacts.push(newContact);
+
+      setDB(accountContacts);
+
+      pvListGenerator(accountContacts);
     }
+
+    bolianRepeat = "";
+  } else {
+    console.log("na Mojod");
+  }
 };
 
 let setDB = (accountContacts) => {
-    localStorage.setItem(
-        `contacts${acconuntNumber}`,
-        JSON.stringify(accountContacts)
-    );
+  localStorage.setItem(
+    `contacts${acconuntNumber}`,
+    JSON.stringify(accountContacts)
+  );
 };
 
 let pvListGenerator = (accountContacts) => {
-    let newPvBoxDivElem, newImageBoxDivElem, newcircleDiv, newInfoDiw, newTexname, newtexEndChat;
+  let newPvBoxDivElem,
+    newImageBoxDivElem,
+    newcircleDiv,
+    newInfoDiw,
+    newTexname,
+    newtexEndChat;
 
-    pvlist.innerHTML = "";
+  pvlist.innerHTML = "";
 
-    accountContacts.forEach((contact) => {
-        newPvBoxDivElem = document.createElement("div");
-        newPvBoxDivElem.classList = "pv";
-        newPvBoxDivElem.setAttribute("value", `${contact.Phone_number}`);
-        newPvBoxDivElem.setAttribute("onclick", `openChatList(${contact.Phone_number})`);
+  accountContacts.forEach((contact) => {
+    newPvBoxDivElem = document.createElement("div");
+    newPvBoxDivElem.classList = "pv";
+    newPvBoxDivElem.setAttribute("value", `${contact.Phone_number}`);
+    newPvBoxDivElem.setAttribute(
+      "onclick",
+      `openChatList(${contact.Phone_number})`
+    );
 
-        newImageBoxDivElem = document.createElement("div");
-        newImageBoxDivElem.classList = "imageprofile";
+    newImageBoxDivElem = document.createElement("div");
+    newImageBoxDivElem.classList = "imageprofile";
 
-        newcircleDiv = document.createElement("div");
-        newcircleDiv.classList = "circle";
+    newcircleDiv = document.createElement("div");
+    newcircleDiv.classList = "circle";
 
-        newInfoDiw = document.createElement("div");
-        newInfoDiw.classList = "infoprofile";
+    newInfoDiw = document.createElement("div");
+    newInfoDiw.classList = "infoprofile";
 
-        newTexname = document.createElement("div");
-        newTexname.classList = "tex";
-        newTexname.innerHTML = `${contact.name}`;
+    newTexname = document.createElement("div");
+    newTexname.classList = "tex";
+    newTexname.innerHTML = `${contact.name}`;
 
-        newtexEndChat = document.createElement("div");
-        newtexEndChat.classList = "tex t";
-        newtexEndChat.innerHTML = "آخرین بیام";
+    newtexEndChat = document.createElement("div");
+    newtexEndChat.classList = "tex t";
+    newtexEndChat.innerHTML = "آخرین بیام";
 
-        newContact = "";
-        newImageBoxDivElem.append(newcircleDiv);
-        newInfoDiw.append(newTexname, newtexEndChat);
-        newPvBoxDivElem.append(newImageBoxDivElem, newInfoDiw);
-        pvlist.append(newPvBoxDivElem);
-    });
+    newContact = "";
+    newImageBoxDivElem.append(newcircleDiv);
+    newInfoDiw.append(newTexname, newtexEndChat);
+    newPvBoxDivElem.append(newImageBoxDivElem, newInfoDiw);
+    pvlist.append(newPvBoxDivElem);
+  });
 };
 
 let addHandeler = () => {
-    let enteredValue = inputadd.value;
+    
+  let enteredValue = inputadd.value;
 
-    if (
-        isNaN(inputadd.value) ||
-        enteredValue.length > 11 ||
-        enteredValue.length < 11
-    ) {
-        error.style.display = "flex";
-    } else {
-        error.style.display = "none";
-        adderNewContact(enteredValue);
-        closeWindowAddHandeler();
-    }
+  if (
+    isNaN(inputadd.value) ||
+    enteredValue.length > 11 ||
+    enteredValue.length < 11
+  ) {
+    error.style.display = "flex";
+  } else {
+    error.style.display = "none";
+    fetchSetContact()/////////////////////////////////////////////
+    adderNewContact(enteredValue);
+    closeWindowAddHandeler();
+  }
 
-    inputadd.value = "";
+  inputadd.value = "";
 };
 
 let closeWindowAddHandeler = () => {
-    container.style.filter = "none";
-    error.style.display = "none";
-    containerAdd.style.display = "none";
-    inputadd.value = "";
+  container.style.filter = "none";
+  error.style.display = "none";
+  containerAdd.style.display = "none";
+  inputadd.value = "";
 };
 
 let butStartAddPvHandler = () => {
-    containerAdd.style.display = "block";
-    container.style.filter = "blur(10px)";
+  containerAdd.style.display = "block";
+  container.style.filter = "blur(10px)";
 };
 
 /////enter in chatList/////////////////////////////////////////////////////////////////////////////////
 
 let openChatList = (s) => {
-    console.log(s);
-    pvActive = s;
-    chats = "";
+  console.log(s);
+  pvActive = s;
+  chats = "";
 
-    starter.style.display = "none";
-    chatroom.style.display = "flex";
-    chatlist.innerHTML = "";
+  starter.style.display = "none";
+  chatroom.style.display = "flex";
+  chatlist.innerHTML = "";
 
-    getChatDB();
+  getChatDB();
 };
 
 /////type chat and save in text
 let addChathandler = (arreyChats) => {
-    let newChatBoxDivElem, newSvgelem, newTexChatDivElem;
-    chatlist.innerHTML = ''
-    arreyChats.forEach((element) => {
-        newChatBoxDivElem = document.createElement("div");
-        newChatBoxDivElem.classList = "texchat to";
+  let newChatBoxDivElem, newSvgelem, newTexChatDivElem;
+  chatlist.innerHTML = "";
+  arreyChats.forEach((element) => {
+    newChatBoxDivElem = document.createElement("div");
+    newChatBoxDivElem.classList = "texchat to";
 
-        newTexChatDivElem = document.createElement("div");
-        newTexChatDivElem.innerHTML = element.tex;
+    newTexChatDivElem = document.createElement("div");
+    newTexChatDivElem.innerHTML = element.tex;
 
-        newSvgelem = document.createElement("svg");
-        newSvgelem.setAttribute("viewBox", "0 0 48 48");
-        newSvgelem.setAttribute("fill", "currentColor");
-        newSvgelem.setAttribute("style", "color: aliceblue;width:22px;");
-        newSvgelem.innerHTML = `<svg viewBox="0 0 48 48" fill="currentColor" style="color: aliceblue;width:22px;padding-top: 8px;" id="ssvg">
+    newSvgelem = document.createElement("svg");
+    newSvgelem.setAttribute("viewBox", "0 0 48 48");
+    newSvgelem.setAttribute("fill", "currentColor");
+    newSvgelem.setAttribute("style", "color: aliceblue;width:22px;");
+    newSvgelem.innerHTML = `<svg viewBox="0 0 48 48" fill="currentColor" style="color: aliceblue;width:22px;padding-top: 8px;" id="ssvg">
           <path d="M12.5,36a1.5,1.5,0,0,1-1.061-.439L1.379,25.5,3.5,23.379l8.9,8.9L33.316,7.387l2.3,1.929L13.648,35.465A1.5,1.5,0,0,1,12.565,36Z"></path>
           <path d="M45.316,7.387,24.4,32.282l-2.972-2.973-1.937,2.306,3.945,3.946A1.5,1.5,0,0,0,24.5,36h.065a1.5,1.5,0,0,0,1.083-.534L47.613,9.316Z"></path>
           </svg>`;
 
-        newChatBoxDivElem.append(newTexChatDivElem, newSvgelem);
-        chatlist.append(newChatBoxDivElem);
-    });
+    newChatBoxDivElem.append(newTexChatDivElem, newSvgelem);
+    chatlist.append(newChatBoxDivElem);
+  });
 };
 
 let addChat = () => {
-    let chat = {
-        tex: `${inputchat.value}`,
-        status: "sent",
-        sender: `"${acconuntNumber}"`,
-    };
+  let chat = {
+    tex: `${inputchat.value}`,
+    status: "sent",
+    sender: `"${acconuntNumber}"`,
+  };
 
-    chats.push(chat);
+  chats.push(chat);
 
-    addChathandler(chats);
+  addChathandler(chats);
 
-    setChatDB(chats);
+  setChatDB(chats);
 
-    inputchat.value = "";
+  inputchat.value = "";
 };
 
 let setChatDB = (chats) => {
-    localStorage.setItem(`chats${pvActive}`, JSON.stringify(chats));
+  localStorage.setItem(`chats${pvActive}`, JSON.stringify(chats));
 };
 let getChatDB = () => {
-    let chatData = JSON.parse(localStorage.getItem(`chats${pvActive}`));
+  let chatData = JSON.parse(localStorage.getItem(`chats${pvActive}`));
 
-    if (chatData) {
-        chats = chatData;
-    } else {
-        chats = [];
-    }
+  if (chatData) {
+    chats = chatData;
+  } else {
+    chats = [];
+  }
 
-    addChathandler(chats);
+  addChathandler(chats);
 };
 /////ButMenumore
 function bodyhandeler() {
-    if (menuMore.style.display == "flex") {
-        menuMore.style.display = "none";
-    }
+  if (menuMore.style.display == "flex") {
+    menuMore.style.display = "none";
+  }
 }
