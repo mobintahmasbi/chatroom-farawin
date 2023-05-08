13087af53d472e97a6bd81d1305131af5d263c13
mobin <mobintahmasbi13801202@gmail.com>
2023-05-08 21:28:47 +0430

    a tiny change in url

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import MessageFunctionalityRouter from "./MessageFunctionality.js";
 
 export default function (app){
     app.use("/api/v1/chatroom/contacts", ContactFunctionlityRouter)
-    app.use("/api/v1/messages", MessageFunctionalityRouter)
+    app.use("/api/v1/chatroom/messages", MessageFunctionalityRouter)
     app.use("/Register", RegisterRouter)
     app.use('/Login', loginrouter)
     app.use("/", ChatroomRouter)
