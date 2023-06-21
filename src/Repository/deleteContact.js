096fb212ce8e57cd57ebf1c804ed74f137e0bc87
Omid <omidk7914.ok@gmail.com>
2023-06-21 23:08:19 +0430

    finish repository deletUser

--- src/Repository/deleteContact.js
+++ src/Repository/deleteContact.js
@@ -1,19 +1,24 @@
 import User from "../models/Users.js";
 
-async function deletContact (UPhoneNumber,CPhoneNumber){
-
-    const user = await User.findOne({Phone_number: UPhoneNumber})
-    const contacts = user.contacts;
-    await contacts.forEach(async (contact) =>{
-        if (contact.Phone_number === CPhoneNumber) {
-            await contact.pull({Phone_number:CPhoneNumber})
-            await user.save()
-            return {
-                status: true
-            }
-        }
-        
-    })
-
-
+async function deletContact(UPhoneNumber, CPhoneNumber) {
+  const userU = await User.findOne({ Phone_number: UPhoneNumber });
+  const userC = await User.findOne({ Phone_number: CPhoneNumber });
+  const contactsU = userU.contacts;
+  const contactsC = userC.contacts;
+  await contactsU.forEach(async (contact) => {
+    if (contact.Phone_number === CPhoneNumber) {
+      await contact.pull({ Phone_number: CPhoneNumber });
+      await userU.save();
+    }
+  });
+  await contactsC.forEach(async (contact) => {
+    if (contact.Phone_number === UPhoneNumber) {
+      await contact.pull({ Phone_number: UPhoneNumber });
+      await userC.save()
+      return {
+        status:true
+      };
+    }
+  });
+  return {status:false}
 }
