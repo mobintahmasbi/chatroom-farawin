621a86960134d4a2c1706160fb3e5448072e0e04
Omid <omidk7914.ok@gmail.com>
2023-06-22 00:12:53 +0430

    router & try catch

--- src/Controllers/ContactController.js
+++ src/Controllers/ContactController.js
@@ -113,42 +113,45 @@ const changeNameContact = async (req, res, next) => {
   }
 };
 
-const deleteContactsC = async (req,res , next) => {
-  const { token } = req.cookies;
-  if (token === undefined) {
-    return res.send({
-      status: false,
-      msg: "user doesn't login !!!",
-    });
-  }
+const deleteContactsC = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (token === undefined) {
+      return res.send({
+        status: false,
+        msg: "user doesn't login !!!",
+      });
+    }
 
-  const { ContactPhoneNumber} = req.body
-  if (ContactPhoneNumber === undefined) {
-    return res.send({
-      status: false,
-      msg: "wrong information!!!",
-    });
-  }
-  const user = jwt.verify(token, privateKey);
-  const existance = await contactExist(user.phoneNumber, ContactPhoneNumber);
+    const { ContactPhoneNumber } = req.body;
+    if (ContactPhoneNumber === undefined) {
+      return res.send({
+        status: false,
+        msg: "wrong information!!!",
+      });
+    }
+    const user = jwt.verify(token, privateKey);
+    const existance = await contactExist(user.phoneNumber, ContactPhoneNumber);
     if (!existance.status) {
       return res.send({
         status: false,
         msg: "مخاطب مورد نظر یافت نشد.",
       });
     }
-    const deleteStat = await deletContact(user.phoneNumber,ContactPhoneNumber);
+    const deleteStat = await deletContact(user.phoneNumber, ContactPhoneNumber);
     if (!deleteStat) {
       return res.send({
         status: false,
         msg: "مشکلی پیش آمده لطفا دوباره تلاش کنید.",
       });
-    };
+    }
     return res.send({
       status: true,
       msg: "کاربر با موفقیت محو شد",
     });
-    
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 
-export { getContactsC, createNewContactC, changeNameContact };
+export { getContactsC, createNewContactC, changeNameContact , deleteContactsC};
--- src/router/ContactFunctionality.js
+++ src/router/ContactFunctionality.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { changeNameContact, createNewContactC, getContactsC } from "../Controllers/ContactController.js";
+import { changeNameContact, createNewContactC, getContactsC , deleteContactsC } from "../Controllers/ContactController.js";
 
 const ContactFunctionalityRouter = Router()
 
 ContactFunctionalityRouter.get("/", getContactsC)
 ContactFunctionalityRouter.post("/", createNewContactC)
 ContactFunctionalityRouter.patch("/", changeNameContact)
+ContactFunctionalityRouter.delete("/", deleteContactsC)
 
 export default ContactFunctionalityRouter
